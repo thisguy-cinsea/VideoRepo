3e80fc3e99b5d98b062b39efcde9ca407f61ab84
Will Ancona <ancona.resume@gmail.com>
2020-03-05 13:14:38 -0500

    first commit

---
+++ src/main/java/com/github/thisguy_cinsea/videorepo/VideoRepoApplication.java
@@ -0,0 +1,13 @@
+package com.github.thisguy_cinsea.videorepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoRepoApplication.class, args);
+    }
+
+}
